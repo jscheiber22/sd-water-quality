0863695bf3262ccf4f2cb596e19f9125cf471440
James Scheiber <jscheiber22@gmail.com>
2022-09-09 16:30:02 -0700

    added file for testing JSON output, seems to work so far but probably isn't conventional

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# in_memory_datastore = {
-#    "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
-#    "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
-#    "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
-# }
 
 @app.get('/sdwaterquality')
 def waterquality():
 	WQ = WaterQuality()
 	WQ.start()
 	return {"water":list(WQ.info.values())}
+
 
 class WaterQuality:
 	def __init__(self):
@@ -43,27 +39,21 @@ class WaterQuality:
 		html = self.grabLines(False)
 
 		# Gathers list of location titles
-		locations = []
+		self.locations = []
 		# Gathers the text associated with each location
-		locationText = []
+		self.locationText = []
 		for x in range(0, len(html)):
 			if '<tr' in html[x]:
 				loc = re.search('sans-serif;">.+</span>', html[x+2]) # Two lines below the <tr> tag is the location title
 				if loc is not None:
-					locations.append(self.cleanText(loc))
+					self.locations.append(self.cleanText(loc))
 				text = re.search('sans-serif;">.+</span>', html[x+3]) # Three lines below the <tr> tag is the location text
 				if text is not None:
-					locationText.append(self.cleanText(text))
+					self.locationText.append(self.cleanText(text))
 
 		# Creates a dictionary out of the cleaned up lists w title as key and text as value
 		# It is wrapped within another list for API purposes
-		self.info = {'qualities': dict(zip(locations[1:], locationText[3:])) }
-
-
-		# for key in self.info:
-		# 	print(key)
-		# 	print(self.info[key])
-		# 	print('\n')
+		self.info = { 'qualities':dict(zip(self.locations[1:], self.locationText[3:])) }
 
 
 if __name__ == '__main__':
