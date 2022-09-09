70ce5549ceb1d82666244383432c2fcb715db3ef
James Scheiber <jscheiber22@gmail.com>
2022-09-09 15:22:03 -0700

    first steps with json implementation

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import subprocess
 import re
+from flask import Flask
 
+
+app = Flask(__name__)
+
+# in_memory_datastore = {
+#    "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
+#    "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
+#    "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
+# }
+
+@app.get('/sdwaterquality')
+def waterquality():
+	WQ = WaterQuality()
+	WQ.start()
+	return {"water":list(WQ.info.values())}
 
 class WaterQuality:
 	def __init__(self):
 		self.url = 'https://www.sdcoastkeeper.org/beach-advisories'
+		self.info = {}
 
 
 	# Disable refresh when referencing a predownloaded site, mostly used for debugging
 	def grabLines(self, refresh = True):
 		if refresh:
 			subprocess.call('curl ' + self.url + ' -o SDBeachAdvisory.html')
-		with open('SDBeachAdvisory.html') as f:
-			return f.readlines()
+		try:
+			with open('SDBeachAdvisory.html') as f:
+				return f.readlines()
+		except FileNotFoundError:
+			print('FileNotFoundError. Could not locate html file.')
 
 
 	def cleanText(self, text):
@@ -36,13 +55,15 @@ class WaterQuality:
 				if text is not None:
 					locationText.append(self.cleanText(text))
 
-		info = dict(zip(locations[1:], locationText[3:]))
+		# Creates a dictionary out of the cleaned up lists w title as key and text as value
+		# It is wrapped within another list for API purposes
+		self.info = {'qualities': dict(zip(locations[1:], locationText[3:])) }
 
 
-		for key in info:
-			print(key)
-			print(info[key])
-			print('\n')
+		# for key in self.info:
+		# 	print(key)
+		# 	print(self.info[key])
+		# 	print('\n')
 
 
 if __name__ == '__main__':
