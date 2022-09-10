9ab98e2c92ef9b48b5fad21b02070cb20a79f739
James Scheiber <jscheiber22@gmail.com>
2022-09-09 17:46:13 -0700

    minor changes but the website managed to drastically change within 24 hours and it broke everything so i fixed that

--- APITest.py
+++ APITest.py
@@ -3,6 +3,5 @@ import requests
 response = requests.get("http://localhost:5000/sdwaterquality")
 out = response.json()["water"]
 
-
 for key in out[0]:
 	print(key + ': ' + out[0][key])
--- main.py
+++ main.py
@@ -22,16 +22,22 @@ class WaterQuality:
 	# Disable refresh when referencing a predownloaded site, mostly used for debugging
 	def grabLines(self, refresh = True):
 		if refresh:
-			subprocess.call('curl ' + self.url + ' -o SDBeachAdvisory.html')
+			subprocess.call('curl ' + self.url + ' -o SDBeachAdvisory.html', shell=True)
 		try:
 			with open('SDBeachAdvisory.html') as f:
-				return f.readlines()
+				lines =  f.readlines()
 		except FileNotFoundError:
 			print('FileNotFoundError. Could not locate html file.')
 
+		if lines is not None:
+			return lines
+		else:
+			print('Preventing a TypeError. An html file was evidently found but seems to be empty and returned type None. Probably need to redonwload the site :).')
+			exit()
+
 
 	def cleanText(self, text):
-		return text.group().replace('sans-serif;">', '').replace('</span>', '').replace('&#8217;', "'")
+		return text.group().replace('sans-serif;">', '').replace('</span>', '').replace('&#8217;', "'").replace('<span style="color: #ff0000;"><span style="font-size: 10pt; font-family: arial, helvetica, sans-serif; color: #000000;">', '')
 
 
 	# Curl the site and decipher the pulled table
@@ -39,21 +45,21 @@ class WaterQuality:
 		html = self.grabLines(False)
 
 		# Gathers list of location titles
-		self.locations = []
+		locations = []
 		# Gathers the text associated with each location
-		self.locationText = []
+		locationText = []
 		for x in range(0, len(html)):
 			if '<tr' in html[x]:
 				loc = re.search('sans-serif;">.+</span>', html[x+2]) # Two lines below the <tr> tag is the location title
 				if loc is not None:
-					self.locations.append(self.cleanText(loc))
+					locations.append(self.cleanText(loc))
 				text = re.search('sans-serif;">.+</span>', html[x+3]) # Three lines below the <tr> tag is the location text
 				if text is not None:
-					self.locationText.append(self.cleanText(text))
+					locationText.append(self.cleanText(text))
 
 		# Creates a dictionary out of the cleaned up lists w title as key and text as value
 		# It is wrapped within another list for API purposes
-		self.info = { 'qualities':dict(zip(self.locations[1:], self.locationText[3:])) }
+		self.info = { 'qualities': dict(zip(locations[1:], locationText[1:])) } # This was previously [3:] until they only had 1 listed beach ("all beaches") but now it only works with [1:] which makes more sense anyway
 
 
 if __name__ == '__main__':
